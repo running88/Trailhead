9b53c2c3e7ed604bfd68e6b3351b3ca2b4e65cba
mritzi <rizwanakhtar08@gmail.com>
2017-05-05 16:20:41 +0530

    Create expensesHelper.js

---
+++ Expense App/expensesHelper.js
@@ -0,0 +1,45 @@
+({
+	createExpense : function(component, expense) {
+		var action = component.get("c.saveExpense");
+        action.setParams({
+            //"expense" should match variable name in Apex function
+            "expense": expense
+        });
+        action.setCallback(this,function(response){
+            var state = reposnse.getState();
+            if(component.isValid() && state === "SUCCESS"){
+                var expenses = component.get("v.expense");
+                expenses.push(response.getReturnValue());
+                component.set("v.expense",expenses);
+            }
+        });
+        $A.enqueueAction(action);
+	},
+    validateExpenseForm : function(component){
+        var validExpense = true;
+        var nameField = component.find("expname");
+        var expname = nameField.get("v.value");
+        if($A.util.isEmpty(expname)){
+            validExpense = false;
+            nameField.set("v.errors",[{
+                message: "Name Field can't be empty"
+            }]);
+        }
+        else
+            nameField.set("v.errors",null);
+        
+        //validate amount
+        var amtField = component.find("expamount");
+        var amt = amtField.get("v.value");
+        if($A.util.isEmpty(amt) || isNaN(amt) || amt<=0.0){
+            validExpense = true;
+            amtField.set("v.errors",[{
+                message: "Enter expense amount"
+            }]);
+        }
+        else
+            amtField.set("v.errors", null);
+        
+        return validExpense;
+    }
+})
